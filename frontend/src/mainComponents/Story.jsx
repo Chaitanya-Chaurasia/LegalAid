e1ad6ffb853f64ba4b97e56d629f71564d381507
AdolfoGzz <118020837+AdolfoGzz@users.noreply.github.com>
2023-10-21 18:05:24 -0400

    edit

--- frontend/src/mainComponents/Story.jsx
+++ frontend/src/mainComponents/Story.jsx
@@ -2,6 +2,9 @@ import Flex from "@chakra-ui/react";
 
 function Story(){
     return(
-        <Flex 
+        <Flex width={'80%'} height={'50px'} backgroundColor={'#E0E0E0'} borderRadius={'20px'}>
+
+        </Flex>
     )
 }
+export default Story
