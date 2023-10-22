707471aca58dbc6d472192a055135f1759be9323
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-10-22 08:51:49 -0400

    Update Chat.jsx

--- frontend/src/mainComponents/Chat.jsx
+++ frontend/src/mainComponents/Chat.jsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 import axios from "axios";
 import Actions from "./Actions";
 
-function Chat({ setActions, setViolations, setLaws }) {
+function Chat({ setActions, setViolations, setLaws, setFirst }) {
   const [message, setMessage] = useState(""); // initialize response state variable
   const [resp, setResponse] = useState("");
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMessage = async () => {
     let res =
@@ -15,6 +17,8 @@ function Chat({ setActions, setViolations, setLaws }) {
     let laws = "The following laws are enforced : \n";
 
     let violations = [];
+
+    setIsLoading(true);
 
     try {
       const response = await axios.get(
@@ -25,12 +29,16 @@ function Chat({ setActions, setViolations, setLaws }) {
         r ? (res += r + " ") : "";
       });
 
-      // response["data"]["laws"].map((l) => {
-      //   // l ? (laws += l + " ") : "";
-      //   for (const property in l) {
-      //     l ? (laws += l + l[property]) : (laws += "");
-      //   }
-      // });
+      response["data"]["laws"].map((l) => {
+        // l ? (laws += l + " ") : "";
+
+        let s =
+          (l ? l.name + " " : " ") +
+          (l ? l.human_right + " " : " ") +
+          (l ? l.description + "\n" : " ");
+
+        laws += s;
+      });
 
       response["data"]["charges"].map((c) => {
         c ? violations.push(c) : violations.push("");
@@ -42,15 +50,16 @@ function Chat({ setActions, setViolations, setLaws }) {
       laws += "Please refer to a legal advisor for acurate information.";
     } catch (err) {
       console.log(err);
+    } finally {
+      setIsLoading(false);
     }
 
     setResponse(res);
 
     setActions(res);
     setLaws(laws);
-    console.log(violations);
     setViolations(violations);
-    setFirst(true);
+    setFirst(false);
   };
 
   const onChange = (e) => {
@@ -85,12 +94,16 @@ function Chat({ setActions, setViolations, setLaws }) {
         {resp && <Response key={resp} message={resp} setActions={setActions} />}
       </Flex>
       <Center height={"60px"} width={"100%"}>
-        <Input
-          placeholder="Ask a question!"
-          backgroundColor={"#F0F0F0"}
-          onChange={onChange}
-          onKeyDown={onKeyDown}
-        />
+        {isLoading ? (
+          <Spinner size="md" color="teal.500" />
+        ) : (
+          <Input
+            placeholder="Ask a question!"
+            backgroundColor={"#F0F0F0"}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+          />
+        )}
       </Center>
     </Flex>
   );
