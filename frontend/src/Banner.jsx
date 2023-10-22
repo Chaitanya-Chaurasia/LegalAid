334bcb6a9a484dd15e1bbc39190257d3139e2928
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-10-22 07:50:27 -0400

    Update Banner.jsx

--- frontend/src/Banner.jsx
+++ frontend/src/Banner.jsx
@@ -5,20 +5,7 @@ import google from "./assets/google.png";
 function Banner({ username }) {
   return (
     <Center backgroundColor={"#E0E0E0"} h={"80px"} gap={5} boxShadow={"md"}>
-      <Flex position={"absolute"} left={"50px"} gap={1.5}>
-        <Text
-          color={"#213A83"}
-          fontStyle={"italic"}
-          fontWeight={"bold"}
-          fontSize={"20px"}
-        ></Text>
-        <Text fontStyle={"italic"} fontSize={"20px"}>
-          <Button colorScheme="whiteAlpha" textColor={"#101010"}>
-            <Image src={google} h={"30px"} pr={"10px"} />
-            Log-in with Google
-          </Button>
-        </Text>
-      </Flex>
+      <Flex position={"absolute"} left={"50px"} gap={1.5}></Flex>
       <Image src={Logo} alt="LegalAid Logo" height={"60px"} />
       <Flex alignItems={"flex-end"}>
         <Heading size="3xl" fontWeight={"bold"} as={"h1"}>
