06554e43eb1ee5c0a9c0868931a1b3277dcd7dbf
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-10-22 07:49:39 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
 import Banner from "./Banner.jsx";
 import { Box } from "@chakra-ui/react";
 import Body from "./Body.jsx";
-import Login from "./Login.jsx";
+import { GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 function App() {
+  const googleAuthenticate = async () => {
+    try {
+      req = await axios.get("127.0.0.1:80/login");
+
+      console.log(req);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const responseMessage = (response) => {
+    console.log(response);
+  };
+  const errorMessage = (error) => {
+    console.log(error);
+  };
   return (
     <Box backgroundColor={"#F0F0F0"} h={"100vh"} minWidth={"1200px"}>
       <Banner username="User" />
+      <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
       <Body />
     </Box>
   );
