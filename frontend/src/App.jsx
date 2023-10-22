e478cebc670add85a306809ddea38b51a53097a4
Adolfo Gonzalez <118020837+AdolfoGzz@users.noreply.github.com>
2023-10-22 02:44:58 -0400

    comps

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Banner from './Banner.jsx'
 import { Box } from '@chakra-ui/react'
 import Body from './Body.jsx'
+import Login from './Login.jsx'
+
 function App() {
   return (
     <Box backgroundColor={"#F0F0F0"} h={'100vh'} minWidth={'1200px'}>
       <Banner />
-      <Body />
+      <Login />
     </Box>
   )
 }
